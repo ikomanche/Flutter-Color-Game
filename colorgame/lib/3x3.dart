04a3a54dcef8e1179361ada4222e988d36532420
STU\1600002148 <kilincarslan98@gmail.com>
2021-04-18 12:31:35 +0300

    Fixed whole page refreshing bug when timer counts. Seperated statefulls. Added button functionality

--- colorgame/lib/3x3.dart
+++ colorgame/lib/3x3.dart
@@ -1,28 +1,31 @@
 import 'dart:async';
 import 'dart:math';
-
 import 'package:flutter/material.dart';
 
-class EasyMode extends StatefulWidget {
+class MainPart extends StatelessWidget {
   @override
-  _EasyModeState createState() => _EasyModeState();
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("3x3 Easy Mode"),
+        centerTitle: true,
+      ),
+      body: Container(
+        //height: double.infinity,
+        child: Column(
+          children: [TimeCounter(), ColorMatch()],
+        ),
+      ),
+    );
+  }
 }
 
-class _EasyModeState extends State<EasyMode> {  
-  getRandomColor() {
-    var randomColorValueR = Random().nextInt(255);
-    var randomColorValueB = Random().nextInt(255);
-    var randomColorValueG = Random().nextInt(255);
-    Color randomCOlor = Color.fromARGB(
-        255, randomColorValueR, randomColorValueB, randomColorValueG);
-    return randomCOlor;
-  }
+class TimeCounter extends StatefulWidget {
+  @override
+  _TimeCounterState createState() => _TimeCounterState();
+}
 
-  getRandomIndex() {
-    var randomIndex = Random().nextInt(8);
-    return randomIndex;
-  }
-  
+class _TimeCounterState extends State<TimeCounter> {
   int _counter = 60;
   Timer _timer;
 
@@ -43,115 +46,141 @@ class _EasyModeState extends State<EasyMode> {
   }
 
   @override
+  void initState() {
+    super.initState();
+    _startTimer();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        height: 100,
+        color: Colors.yellow,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Text(
+              'TIME :',
+              textAlign: TextAlign.left,
+              style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 48,
+                  wordSpacing: 5,
+                  letterSpacing: 15),
+            ),
+            ElevatedButton(
+              onPressed: null,
+              child: Text(
+                '$_counter',
+                textAlign: TextAlign.left,
+                style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 60,
+                    wordSpacing: 20,
+                    letterSpacing: 10,
+                    color: Colors.red),
+              ),
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all(Colors.transparent),
+                minimumSize: MaterialStateProperty.all(Size(150, 90)),
+                //side: MaterialStateProperty.all(BorderSide(width: 5)),
+              ),
+            )
+          ],
+        ));
+  }
+}
+
+class ColorMatch extends StatefulWidget {
+  @override
+  _ColorMatchState createState() => _ColorMatchState();
+}
+
+class _ColorMatchState extends State<ColorMatch> {
+  getRandomColor() {
+    var randomColorValueR = Random().nextInt(255);
+    var randomColorValueB = Random().nextInt(255);
+    var randomColorValueG = Random().nextInt(255);
+    Color randomCOlor = Color.fromARGB(
+        255, randomColorValueR, randomColorValueB, randomColorValueG);
+    return randomCOlor;
+  }
+
+  getRandomIndex() {
+    var randomIndex = Random().nextInt(8);
+    return randomIndex;
+  }
+  
+  @override
   Widget build(BuildContext context) {
     Color mainColor = getRandomColor();
     int randomIndex = getRandomIndex();
-
-    return Scaffold(
-        appBar: AppBar(
-          title: Text("3x3 Easy Mode"),
-          centerTitle: true,
-        ),
-        body: Container(
-          height: double.infinity,
-          //color: Colors.green,
-          child: Column(
-            children: [
-              Container(
-                height: 200,
-                color: Colors.yellow,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
-                        Text(
-                          'TIME :',
-                          textAlign: TextAlign.left,
-                          style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 48,
-                              wordSpacing: 5,
-                              letterSpacing: 15),
-                        ),
-                        ElevatedButton(
-                          onPressed: null,
-                          child: Text(
-                            '$_counter',
-                            textAlign: TextAlign.left,
-                            style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                                fontSize: 60,
-                                wordSpacing: 20,
-                                letterSpacing: 10,
-                                color: Colors.red),
-                          ),
-                          style: ButtonStyle(
-                            backgroundColor:
-                                MaterialStateProperty.all(Colors.transparent),
-                            minimumSize:
-                                MaterialStateProperty.all(Size(150, 90)),
-                            //side: MaterialStateProperty.all(BorderSide(width: 5)),
-                          ),
-                        )
-                      ],
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
-                        Text(
-                          'NEXT ',
-                          textAlign: TextAlign.left,
-                          style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 48,
-                              wordSpacing: 20,
-                              letterSpacing: 15),
-                        ),
-                        ElevatedButton(
-                          onPressed: null,
-                          child: null,
-                          style: ButtonStyle(
-                            backgroundColor:
-                                MaterialStateProperty.all(mainColor),
-                            minimumSize:
-                                MaterialStateProperty.all(Size(150, 90)),
-                            side:
-                                MaterialStateProperty.all(BorderSide(width: 5)),
-                          ),
-                        )
-                      ],
-                    ),
-                  ],
-                ),
-              ),
-              Container(
-                  height: 510,
-                  //color: Colors.blue,
-                  child: GridView.count(
-                    crossAxisCount: 3,
-                    primary: false,
-                    padding: const EdgeInsets.all(20),
-                    crossAxisSpacing: 10,
-                    mainAxisSpacing: 10,
-                    children: List.generate(9, (index) {
-                      return ElevatedButton(
-                        onPressed: () => _startTimer(),
+    return Container(
+      height: 620,
+      //color: Colors.blue,
+      child: Column(
+        children: [
+          Container(
+              height: 100,
+              color: Colors.yellow,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Text(
+                        'NEXT ',
+                        textAlign: TextAlign.left,
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 48,
+                            wordSpacing: 20,
+                            letterSpacing: 15),
+                      ),
+                      ElevatedButton(
+                        onPressed: null,
                         child: null,
                         style: ButtonStyle(
-                          backgroundColor: MaterialStateProperty.all(
-                              randomIndex == index
-                                  ? mainColor
-                                  : getRandomColor()),
+                          backgroundColor: MaterialStateProperty.all(mainColor),
+                          minimumSize: MaterialStateProperty.all(Size(150, 90)),
                           side: MaterialStateProperty.all(BorderSide(width: 5)),
                         ),
-                      );
-                    }),
-                  ))
-            ],
-          ),
-        ));
+                      )
+                    ],
+                  )
+                ],
+              )),
+          Container(
+            height: 520,
+            child: GridView.count(
+              crossAxisCount: 3,
+              primary: false,
+              padding: const EdgeInsets.all(20),
+              crossAxisSpacing: 10,
+              mainAxisSpacing: 10,
+              children: List.generate(9, (index) {
+                return ElevatedButton(
+                  onPressed: randomIndex == index ? () {
+                    setState(() {
+                      mainColor = getRandomColor();
+                      randomIndex = getRandomIndex();
+                    });
+                  } : (){},
+                  child: null,
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(
+                        randomIndex == index ? mainColor : getRandomColor()),
+                    side: MaterialStateProperty.all(BorderSide(width: 5)),
+                  ),
+                );
+              }),
+            ),
+            //color: Colors.green,
+          )
+        ],
+      ),
+    );
   }
 }
